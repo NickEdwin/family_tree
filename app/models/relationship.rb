4b5cdb6f26980c75b14eee39217f2b20c72ad894
Nick Edwin <nickedwin85@gmail.com>
2021-04-27 12:46:03 -0600

    Relationships model validations with associations

---
+++ app/models/relationship.rb
@@ -0,0 +1,10 @@
+class Relationship < ApplicationRecord
+  validates_presence_of :family_id,
+                        :family_member_1_id,
+                        :family_member_2_id,
+                        :family_member_1_relationship_to_2,
+                        :family_member_2_relationship_to_1
+
+  belongs_to :family
+  belongs_to :family_member
+end
